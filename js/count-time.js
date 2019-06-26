c49bec66724ec93600b0284b4604b19cdab81e0d
Glouder <764974122@qq.com>
2019-06-26 09:48:00 +0800

    Update count-time.js

--- js/count-time.js
+++ js/count-time.js
@@ -59,3 +59,33 @@
  	var seconds = Math.round(leave3 / 1000);
  	return new Array(days, hours, minutes, seconds);
  }
+
+var audioAutoPlay = function (id){
+
+var audio = document.getElementById(id),
+
+play = function(){
+
+audio.play();
+
+document.removeEventListener("touchstart",play, false);
+
+};
+
+audio.play();
+
+document.addEventListener("WeixinJSBridgeReady", function () {
+
+play();
+
+}, false);
+
+document.addEventListener('YixinJSBridgeReady', function() {
+
+play();
+
+}, false);
+
+document.addEventListener("touchstart",play, false);
+
+}
